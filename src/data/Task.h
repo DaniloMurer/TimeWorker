9f781b070dd162048c77338fd6ce5b9dafda7803
Danilo Jakob <danilo.murer@gmail.com>
2020-08-10 21:30:32 +0200

    CHANGE: Added Documentation for the Task class

--- src/data/Task.h
+++ src/data/Task.h
@@ -10,21 +10,71 @@
 
 class Task {
 private:
+
+    /**
+     * Task Id
+     */
     int *id;
+
+    /**
+     * Task name
+     */
     std::string *taskName;
+
+    /**
+     * Last start of the task
+     */
     time_t *taskTime;
+
+    /**
+     * Logged hours for the task
+     */
     int *loggedHours;
+
 public:
+
+    /**
+     * Get id from Task
+     * @return The Id of the task
+     */
     int* get_id();
+
+    /**
+     * Get the time of the task
+     * @return Time of the task
+     */
     time_t* get_time();
+
+    /**
+     * Get the name of the task
+     * @return The name of the task
+     */
     std::string* get_name();
+
+    /**
+     * Get the logged time for the task
+     * @return The logged time
+     */
     int* get_logged_time();
+
     /**
      * Destructor for Task class for deleting all pointer references
      */
     ~Task();
 
+    /**
+     * Constructor for auto generating task with the name only
+     * @param taskName The name of the task
+     */
     explicit Task(std::string taskName);
+
+    /**
+     * Constructor for creating a task with all properties
+     * @param id Id of the task
+     * @param taskName Name of the task
+     * @param loggedHours Logged Hours for the task
+     * @param taskTime Time of the task
+     */
     explicit Task(int id, std::string taskName, int loggedHours, time_t taskTime);
 };
 
